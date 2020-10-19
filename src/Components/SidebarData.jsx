b8a7cab069a9ebf1bfed350577c4c8cc9a2b6274
nativeKar <thakuryogeshsingh@gmail.com>
2020-10-20 01:43:14 +0530

    fix: refactored icons

--- src/Components/SidebarData.jsx
+++ src/Components/SidebarData.jsx
@@ -1,24 +1,23 @@
 import React from "react";
-import * as AiIcons from "react-icons/ai";
-import * as IoIcons from "react-icons/io";
+import * as BiIcons from "react-icons/bi";
 
 export const SidebarData = [
   {
     title: "Chart Selector",
     path: "/",
-    icon: <AiIcons.AiFillHome />,
+    icon: <BiIcons.BiHome />,
     className: "nav-text",
   },
   {
     title: "Vertical Bar Chart",
     path: "/vertical-bar-chart",
-    icon: <IoIcons.IoIosPaper />,
+    icon: <BiIcons.BiBarChartAlt2 />,
     className: "nav-text",
   },
   {
     title: "Donut Chart",
     path: "/donut-chart",
-    icon: <IoIcons.IoIosPaper />,
+    icon: <BiIcons.BiChart />,
     className: "nav-text",
   },
 ];
